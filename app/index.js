1bf540b9117bc9842eb7e5a020ca4397cccc4b98
Leland Jordon <lelandjordon@mac.com>
2016-05-25 21:42:40 -0400

    added comment component and it renders. Made notes for self-reference.

--- app/index.js
+++ app/index.js
@@ -16,27 +16,32 @@ var post = {
   ]
 }
 
+var Comment = React.createClass({   // this component creates Comment, which gets used
+  render: function(){             // later in the Post component.
+    return (
+      <p>{this.props.commentBody}</p> // this is what gets sent to the DOM.
+    )
+  }
+});
 
 var Post = React.createClass({
   render: function(){
+    var commentsList = this.props.comments.map(function(comment, index){ // instead of a For loop,
+      return <Comment key={index} commentBody={comment} />    // it passes each comment's body and index
+    })                                                        // into Comment, which was created above.
     return (
       <div>
         <h1>{this.props.title}</h1>
         <p>By: {this.props.author}</p>
         <p>{this.props.body}</p>
         <h2>Comments</h2>
-          var comment = function(comment){
-             Comment = [];
-          for(i=0; i<=comment.length; i++)
-            <p>{this.props.comment}</p>
-          };
-      };
+        {commentsList}    // this is where the commentsList gets rendered in the DOM.
       </div>
     )
   }
 });
 
 ReactDOM.render(
-  <Post title={post.title} author={post.author} body={post.body} comment={post.comments[0]} />,
+  <Post title={post.title} author={post.author} body={post.body} comments={post.comments} />,
   document.getElementById("app")
 );
